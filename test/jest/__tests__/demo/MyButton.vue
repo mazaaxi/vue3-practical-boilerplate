faab12cf91b3f5efcdf554ecd444d0e48774fedd
mazaaxi <22339605+mazaaxi@users.noreply.github.com>
2021-10-25 19:44:36 +0900

    feat: Install @quasar/testing

---
+++ test/jest/__tests__/demo/MyButton.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p class="content">{{ input }}</p>
+    <span>{{ counter }}</span>
+    <q-btn class="button" @click="increment()"></q-btn>
+  </div>
+</template>
+
+<script lang="ts" src="./MyButton.ts"></script>
