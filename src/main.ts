04ba876c2c688a36afcc880ce408e7cd21d5ea73
mazaaxi <22339605+mazaaxi@users.noreply.github.com>
2022-10-18 23:03:42 +0900

    Introduced Quasar
    refs: ["VUE3-BP-T-5"]

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,38 @@
 import { createApp } from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
-
 import '@/assets/main.css'
+import { Quasar } from 'quasar'
+import { Dialog, Loading, Notify } from 'quasar'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons-outlined/material-icons-outlined.css'
+import '@quasar/extras/material-icons-round/material-icons-round.css'
+import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
+import '@quasar/extras/material-symbols-outlined/material-symbols-outlined.css'
+import '@quasar/extras/material-symbols-rounded/material-symbols-rounded.css'
+import '@quasar/extras/material-symbols-sharp/material-symbols-sharp.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
-
-app.use(router)
+  .use(router)
+  .use(Quasar, {
+    // import Quasar plugins and add here
+    plugins: {
+      Dialog,
+      Loading,
+      Notify,
+    },
+    config: {
+      screen: {
+        bodyClasses: true,
+      },
+      notify: {},
+      loading: {},
+    },
+  })
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,36 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
-</template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
+<style lang="scss" scoped>
+.AboutView {
+  body.screen--lg &,
+  body.screen--xl &,
+  body.screen--md & {
     min-height: 100vh;
     display: flex;
     align-items: center;
   }
 }
+
+.message {
+  display: flex;
+  align-items: center;
+
+  body.screen--xs &,
+  body.screen--sm & {
+    margin-top: 20px;
+  }
+}
 </style>
+
+<template>
+  <div class="AboutView">
+    <div class="message">
+      <q-btn class="text-h5 q-mr-sm" icon="info" @click="iconOnClick" />
+      <span class="text-h3">This is an about page</span>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+function iconOnClick() {
+  console.log('iconOnClick')
+}
+</script>
